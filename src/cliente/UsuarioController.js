595372e070fb92d58da893582f8c5a4154b486c8
Alejandro Michelena <alejandro@Alejandros-MacBook-Pro.local>
2026-05-22 11:36:34 -0400

    progreso Alejandro

---
+++ src/cliente/UsuarioController.js
@@ -0,0 +1,92 @@
+import { UsuarioModel } from "./UsuarioModel.js";
+
+//este controlador manejara 3 archivos html 
+//Iniciar Sesion, Registrar Perfil, Consultar Perfil
+
+export class UsuarioController {
+    constructor() {
+        //referencias a los elementos html:
+        //botones de Registrar Perfil
+        this.inputNombre = document.getElementById("registro-nombre");
+        this.inputEmail = document.getElementById("registro-email");
+        this.inputPassword = document.getElementById("registro-password");
+        this.btnRegistrar = document.getElementById("btn-registrar");
+
+        //botones de Iniciar Sesion
+        this.inputLoginEmail = document.getElementById("login-email");
+        this.inputLoginPassword = document.getElementById("login-password");
+        this.btnLogin = document.getElementById("btn-login");
+
+        //textos donde se mostraran los datos al Consultar Perfil
+        this.txtPerfilNombre = document.getElementById("perfil-nombre");
+        this.txtPerfilEmail = document.getElementById("perfil-email");
+
+        this.configurarEventos();
+    }
+
+    //esta funcion determina en cual de los html estamos cada vez que el html se recargue
+    //ademas ella activa los botones relevantes
+    configurarEventos() {
+        if (this.btnRegistrar) {
+            this.btnRegistrar.addEventListener("click", (e) => this.manejarRegistro(e));
+        }
+        if (this.btnLogin) {
+            this.btnLogin.addEventListener("click", (e) => this.manejarLogin(e));
+        }
+        if (this.txtPerfilNombre) {
+            this.cargarPerfil();
+        }
+    }
+
+    manejarRegistro(e) {
+        e.preventDefault(); //evita que se reinicie el html
+        
+        const nombre = this.inputNombre.value;
+        const email = this.inputEmail.value;
+        const password = this.inputPassword.value;
+
+        const error = UsuarioModel.validarDatos(email, password); //funcion nativa del objeto Usuario (BOOL)
+        if (error) {
+            alert(error); /////
+            return;
+        }
+
+        const exito = UsuarioModel.guardarUsuario(nombre, email, password);
+        if (exito) {
+            alert("Registro exitoso");
+        } else {
+            alert("El correo ya está registrado");
+        }
+    }
+
+    manejarLogin(e) {
+        e.preventDefault();
+
+        const email = this.inputLoginEmail.value;
+        const password = this.inputLoginPassword.value;
+
+        const usuario = UsuarioModel.verificarCredenciales(email, password);
+        if (usuario) {
+            sessionStorage.setItem("usuarioActivo", JSON.stringify(usuario));
+            window.location.href = "gestionProductos.html";
+        } else {
+            alert("Credenciales incorrectas");
+        }
+    }
+
+    cargarPerfil() {
+        const sesion = sessionStorage.getItem("usuarioActivo");
+        if (!sesion) {
+            window.location.href = "login.html";
+            return;
+        }
+
+        const usuario = JSON.parse(sesion); //objeto plano de JS pero contiene todos los datos del usuario
+        this.txtPerfilNombre.textContent = usuario.nombre;
+        this.txtPerfilEmail.textContent = usuario.email;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    new UsuarioController();
+});
--- src/cliente/UsuarioModel.js
+++ src/cliente/UsuarioModel.js
@@ -9,17 +9,30 @@ export class Usuario {
     }
 
     static validarDatos(nombre, email, password) {
-    if (!nombre || !email || !password) {
-        return "Todos los campos son obligatorios";
+        if (!nombre || !email || !password) {
+            return "Todos los campos son obligatorios";
+        }
+        const regexNombre = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]{3,15} [a-zA-ZáéíóúÁÉÍÓÚñÑ]{3,15}$/;
+        if(!regexNombre.test(nombre)) {
+            return "Debe seguir la estructura: primer nombre, un espacio y el apellido"
+        }
+        const regexUcab = /^[a-zA-Z0-9._%+-]{3,35}@est\.ucab\.edu\.ve$/;
+        if (!regexUcab.test(email)) {
+            return "Debes usar un correo institucional valido (@est.ucab.edu.ve)";
+        }
+        const regexPassword = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[.\\-_/*+!?¿¡@#$%^&]).{6,}$/;
+        if (!regexPassword.test(password)) {
+            return "La contraseña debe tener entre [6-15] caracteres, contener al menos una letra, un número y un carácter especial";
+        }
+        return null; // null == todo bien
     }
-    const regexUcab = /^[a-zA-Z0-9._%+-]{3,35}@est\.ucab\.edu\.ve$/;
-    if (!regexUcab.test(email)) {
-        return "Debes usar un correo institucional valido (@est.ucab.edu.ve)";
+
+    //implementar mas tarde ************* (de la mano con el JSON)
+    static guardarUsuario(nombre, correo, password) {
+        //RECORDAR QUE DEVUELVE UN BOOLEAN DE CONFIRMACION
     }
-    const regexPassword = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[.\\-_/*+!?¿¡@#$%^&]).{6,}$/;
-    if (!regexPassword.test(password)) {
-        return "La contraseña debe tener entre [6-15] caracteres, contener al menos una letra, un número y un carácter especial";
-    }
-    return null; // null == todo bien
+
+    static verificarCredenciales(email, password) {
+        //RECORDAR QUE DEVUELVE AL USUARIO (O NULL EN CASO DE NO EXISTIR)
     }
 }
